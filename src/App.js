c48e57dff4cd6ae5ee0d9186cd77e4aa4fd15daa
SofiaPutlyuk <sofiaputlyuk@gmail.com>
2025-04-24 21:49:12 +0300

    work with cartoons,form and map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Map from './components/Map';
+import Form from './components/Form';
 function App() {
   return (
+  <div className='container'>
+    <Form />
 <Map />
+</div>
   );
 }
 
